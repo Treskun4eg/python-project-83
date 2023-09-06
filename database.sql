cb5e8a59ee1f012f99095d85ca9c37a06a665eed
Treskun4eg <wagamamanowelli@gmail.com>
2023-09-06 19:59:57 +0200

    add: add table url_checks

--- database.sql
+++ database.sql
@@ -1,5 +1,20 @@
+DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
+
+
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) UNIQUE NOT NULL,
+    created_at date NOT NULL
+);
+
+
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id varchar(255) NOT NULL REFERENCES urls (id),
+    status_code integer,
+    h1 varchar(255),
+    title varchar(255),
+    description text,
     created_at date NOT NULL
 );
