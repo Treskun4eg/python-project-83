9162c73c57dca9dfc8050be4196a9417f6e683aa
Treskun4eg <wagamamanowelli@gmail.com>
2023-10-08 23:49:25 +0200

    add: url validator

---
+++ page_analyzer/validator.py
@@ -0,0 +1,24 @@
+from urllib.parse import urlparse
+import validators
+
+
+def extract_domain_and_normalize(data):
+    url = data['url']
+    parse_url = urlparse(url)
+    normalized_url = parse_url.scheme + '://' + parse_url.netloc
+    return normalized_url
+
+
+def validation_url(data):
+    errors = {}
+    url = data['url']
+    get_a_domain = extract_domain_and_normalize(data)
+    url_valid = validators.url(url)
+    if not url_valid and len(get_a_domain) > 255:
+        errors["VarcharERROR1"] = "URL превышает 255 символов"
+    if not url_valid and len(url) == 0:
+        errors["VarcharERROR2"] = "Некорректный URL"
+        errors["VarcharERROR3"] = "URL обязателен"
+    if not url_valid and 255 > len(url) > 0:
+        errors["VarcharERROR2"] = "Некорректный URL"
+    return errors
