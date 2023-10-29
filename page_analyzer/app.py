d2a2a9fd10424f79964f8dc28cae409f22a4980a
Treskun4eg <wagamamanowelli@gmail.com>
2023-10-29 18:59:33 +0100

    add module with configurations

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,24 +8,20 @@ from flask import (
     get_flashed_messages
 )
 
+from page_analyzer.config import Config
+
 from page_analyzer.validator import validation_url
 from page_analyzer import db
-from page_analyzer.get_seo import get_seo
+from page_analyzer.html import get_seo
 
-import os
 from dotenv import load_dotenv
 from datetime import date
-import psycopg2
 import requests
 
 
 load_dotenv()
 app = Flask(__name__)
-TIMEOUT = int(os.getenv('EXTERNAL_REQUEST_TIMEOUT', 30))
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL')
-connect = psycopg2.connect(DATABASE_URL)
-connect.autocommit = True
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -35,7 +31,7 @@ def index():
 
 @app.get('/urls')
 def list_urls():
-    with connect as conn:
+    with app.config['CONNECT'] as conn:
         urls = db.get_urls(conn)
         url_checks = {
             item.url_id: item for item in db.get_last_URL_check(conn)
@@ -49,27 +45,28 @@ def list_urls():
 def add_url():
 
     url_name = request.form.to_dict()
-    errors = validation_url(url_name)
+    url = url_name['url']
+    errors = validation_url(url)
 
     if errors:
         for key, value in errors.items():
             flash(value, key)
         return render_template('index.html'), 422
 
-    with connect as conn:
-        url_check = db.get_url_id_by_name(url_name, conn)
+    with app.config['CONNECT'] as conn:
+        url_check = db.get_url_id_by_name(url, conn)
         if url_check:
             flash('Страница уже существует', 'Повторение')
             return redirect(url_for('get_url_id', id=url_check.id))
-        db.created_url(url_name, conn)
-        url_id = db.get_url_id_by_name(url_name, conn)
+        db.created_url(url, conn)
+        url_id = db.get_url_id_by_name(url, conn)
         flash('Страница успешно добавлена', 'Успех')
         return redirect(url_for('get_url_id', id=url_id.id))
 
 
 @app.get('/urls/<int:id>')
 def get_url_id(id):
-    with connect as conn:
+    with app.config['CONNECT'] as conn:
         url_info = db.get_url_by_id(id, conn)
         checks_info = db.get_url_checks_by_url_id(id, conn)
         messages = get_flashed_messages(with_categories=True)
@@ -83,11 +80,11 @@ def get_url_id(id):
 def check_url(id):
     date_today = date.today()
 
-    with connect as conn:
+    with app.config['CONNECT'] as conn:
 
         url_info = db.get_url_by_id(id, conn)
         try:
-            request = requests.get(url_info.name, timeout=TIMEOUT)
+            request = requests.get(url_info.name, timeout=app.config['TIMEOUT'])
             request.raise_for_status()
         except requests.exceptions.RequestException as err:
             flash('Произошла ошибка при проверке', 'Ошибка')
