6485c32fd7efa34193ecb5bbf92cd8a0bb40255f
Treskun4eg <wagamamanowelli@gmail.com>
2023-10-29 20:17:19 +0100

    fix db error

--- page_analyzer/config.py
+++ page_analyzer/config.py
@@ -5,5 +5,4 @@ import psycopg2
 class Config(object):
     TIMEOUT = int(os.getenv('EXTERNAL_REQUEST_TIMEOUT', 30))
     SECRET_KEY = os.getenv('SECRET_KEY')
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    CONNECT = psycopg2.connect(DATABASE_URL)
+    CONNECT = psycopg2.connect(os.getenv('DATABASE_URL'))
